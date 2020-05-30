a82aec6597b1048a5b0b86ad4307d8d66ca58c36
Jackson Henry <jackson.henry3@gmail.com>
2020-05-30 12:21:13 -0700

    fixed numers on mobile

--- js/generate_board.js
+++ js/generate_board.js
@@ -10,6 +10,7 @@ for (var III = board.length - 1; III >= 0; III--) {
 		board[III][JJJ] = new square(III,JJJ,colors[5])
 		// board[III][JJJ].element.style.width = squarewidth+"px"
 		board[III][JJJ].element.style.height = board[III][JJJ].element.offsetWidth+"px"
+		board[III][JJJ].text_element.style.lineHeight = board[III][JJJ].element.offsetWidth+"px"
 		// board[III][JJJ].element.style.top = 100*III
 		// board[III][JJJ].element.style.left = 100*JJJ
 	}
